6379605b1efc055881e11ccd0be3f30a76d7ff07
mohan338 <sahumohan338@gmail.com>
2022-05-26 18:59:39 +0530

    [mohan]add:CreateBinaryTree

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using BinaryTree;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" Welcome to Binary Search Tree");
+        BinaryTree<int> binarysearch = new BinaryTree<int>(56);
+        binarysearch.Insert(30);
+        binarysearch.Insert(70);
+        binarysearch.Display();
+
+    }
+}
